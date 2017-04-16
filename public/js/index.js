5f605fdd2d5f532f4983186ad14f6df5d1090cc7
Muirhead-Gould <smuirhe1@ca.corp.zayo.com>
2017-04-16 16:55:10 -0400

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function (){
+  console.log('connected to server');
+
+  socket.emit('createMessage', {
+    from: 'stephen',
+    text: 'hey, this is me'
+  });
+});
+
+socket.on('disconnect', function (){
+  console.log('disconnected from server');
+});
+
+socket.on('newMessage', function (message){
+  console.log('new message', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'mike',
+    text: 'hey, whats up with you',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (message)=>{
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', ()=>{
     console.log('disconnected from server');
   });
