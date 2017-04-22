491afa8c723b8b8c4b5ca4a5a7190a3f53195a02
Muirhead-Gould <smuirhe1@ca.corp.zayo.com>
2017-04-22 15:01:20 -0400

    add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,54 @@
 var socket = io();
 
-socket.on('connect', function (){
+socket.on('connect', function () {
   console.log('connected to server');
-
 });
 
-socket.on('disconnect', function (){
-  console.log('disconnected from server');
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message){
-  console.log('new message', message);
+socket.on('newMessage', function (message) {
+  console.log('newmessage', message);
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`)
+  li.text(`${message.from}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', function (e){
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My Current Location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+
   socket.emit('createMessage', {
       from: "User",
       text: jQuery('[name=message]').val()
   }, function (data){
     console.log('got it', data);
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location');
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', ()=>{
   it('should generate the correct message object', ()=>{
@@ -10,5 +10,19 @@ describe('generateMessage', ()=>{
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from,text});
+  });
+});
+
+describe('generateLocationMessage', ()=>{
+  it('should generate correct location message', ()=>{
+    var from = 'Piper';
+    var latitude = 44;
+    var longitude = 55;
+    var url = 'https://www.google.com/maps?q=44,55';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from,url});
+
   });
 });
